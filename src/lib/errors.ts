62074bd6ce73dd77d45d2001f5d9ff3c1d64839a
Satyapal Sharma <satyapal.info@gmail.com>
2024-08-17 18:47:16 +0000

    refactor: update src/lib/errors.ts

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,7 +1,10 @@
 export class AppError extends Error {
-  constructor(message: string, public statusCode: number = 500) {
+  statusCode: number
+
+  constructor(message: string, statusCode: number = 500) {
     super(message)
     this.name = this.constructor.name
+    this.statusCode = statusCode
   }
 }
 
